59952cbe148f55e751905f80ee7887df9869e839
江恒 <wushaofeng.wsf@alibaba-inc.com>
2023-09-08 10:46:15 +0800

    perf(service): 完善工程结构

--- hexagonal-adaptor/src/main/java/com/acmday/hexagonal/architecture/demo/adaptor/driver/facade/UserInfoFacadeImpl.java
+++ hexagonal-adaptor/src/main/java/com/acmday/hexagonal/architecture/demo/adaptor/driver/facade/UserInfoFacadeImpl.java
@@ -1,8 +1,11 @@
 package com.acmday.hexagonal.architecture.demo.adaptor.driver.facade;
 
+import com.acmday.hexagonal.architecture.demo.adaptor.driven.application.IUserInfoApp;
 import com.acmday.hexagonal.architecture.demo.client.IUserInfoFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+
+import javax.annotation.Resource;
 
 /**
  * @author acmday
@@ -13,6 +16,9 @@ import org.apache.dubbo.config.annotation.DubboService;
 @DubboService(version = "1.0", group = "acmday")
 @Slf4j
 public class UserInfoFacadeImpl implements IUserInfoFacade {
+
+    @Resource
+    private IUserInfoApp userInfoApp;
 
     @Override
     public String getNameByUserId(Long uid) {
